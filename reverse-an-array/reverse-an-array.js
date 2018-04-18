f84619636a3f2ec98c576f6ea8694c188464e618
Joanna Coll <johannam999@gmail.com>
2018-04-17 18:42:03 -0700

    jpg

--- reverse-an-array/reverse-an-array.js
+++ reverse-an-array/reverse-an-array.js
@@ -1,11 +1,11 @@
-function reverseArr (array){
-  let newArr = [];
+function reverseArr(array) {
+  const newArr = [];
   
-  for (let i = array.length-1; i>=0; i--){
-    let elem = array.pop();
+  for (let i = array.length - 1; i >= 0; i--) {
+    const elem = array.pop();
     newArr.push(elem);
   }
   return newArr;
 }
 
-reverseArr(['a','b', 'c', 'd', 'e', 'f']);
+reverseArr(['a', 'b', 'c', 'd', 'e', 'f']);
