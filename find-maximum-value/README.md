6732eb70046894c21054cf298a8d702d05f5a2f2
Joanna Coll <johannam999@gmail.com>
2018-05-10 08:45:33 -0700

    partially done

---
+++ find-maximum-value/README.md
@@ -0,0 +1,6 @@
+# find-max-value
+
+ 
+
+## Solution
+![find-max-value](assets/find-max-value.JPG)
---
+++ find-maximum-value/traversal.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+const findMaxValue = (tree) => {
+  if (!tree.root) {
+    return undefined;
+  }
+  let maxValue = 0;
+
+  const findMaxValueTRaversal = (rootNode) => {
+    if (!rootNode) {
+      return undefined;
+    }
+
+    if (rootNode.value > maxValue) {
+      maxValue = rootNode.value;
+    }
+      findMaxValueTRaversal(rootNode.left);
+
+      findMaxValueTRaversal(rootNode.right);
+    }
+
+
+  return maxValue;
+};
+
+
+export default findMaxValue;
