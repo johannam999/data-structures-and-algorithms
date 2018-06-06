568571c08e936bbf213be6e04fc2f88bc3db4ce7
Joanna Coll <johannam999@gmail.com>
2018-06-05 23:17:37 -0700

    more changes

--- left-join/left-join.js
+++ left-join/left-join.js
@@ -2,17 +2,15 @@
 'use strict';
 
 const leftJoin = (mapA, mapB) => {
-  if (!mapA) return null;
-  if (!mapB) return mapA;
+  if (!mapA) return undefined;
+  if (!mapB) return undefined;
 
   const result = {};
   Object.keys(mapA).forEach((key) => {
     result[key] = [mapA[key]];
     if (mapB[key]) {
       result[key].push(mapB[key]);
-    } else {
-      result[key].push(null);
-    }
+    } 
   });
   return result;
 };
