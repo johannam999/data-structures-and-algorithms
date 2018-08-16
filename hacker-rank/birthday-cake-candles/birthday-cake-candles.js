ed9b3af63f51dd937c8a88ec2d2af9af6c1ed97a
Joanna Coll <35988918+johannam999@users.noreply.github.com>
2018-08-15 19:18:02 -0700

    Update birthday-cake-candles.js

--- hacker-rank/birthday-cake-candles/birthday-cake-candles.js
+++ hacker-rank/birthday-cake-candles/birthday-cake-candles.js
@@ -5,7 +5,7 @@ function birthdayCakeCandles(ar) {
   const candlesCount = ar.filter(candle => candle === maxHeight); 
   return candlesCount.length;
 }
-
+// SOLUTION 2
 
 function birthdayCakeCandle(ar) {
   let currentMax = 0;
@@ -14,13 +14,27 @@ function birthdayCakeCandle(ar) {
     if (ar[i] > currentMax) {
       currentMax = ar[i];  
     } 
-    for (let j = 0; j < ar.length; j++) {
-      if (ar[j] === currentMax) {
-        count += 1;
-      }  
+  }
+  for (let j = 0; j < ar.length; j++) {
+    if (ar[j] === currentMax) {
+      count += 1;
+    }  
+  } 
+  return count;
+}
+
+// SOLUTION 3
+
+function birthdayCakesCandles(ar) {
+  const maxHeight = Math.max(...ar);
+  let count = 0;
+  for (let i = 0; i < ar.length; i++) {
+    if (ar[i] === maxHeight) {
+      count++;  
     }
   }
   return count;
 }
 birthdayCakeCandle([1, 2, 3, 4, 5, 5, 7]);
 birthdayCakeCandles([1, 2, 3, 4, 5, 5, 7]);
+birthdayCakesCandles([1, 2, 3, 4, 5, 5, 7]);
