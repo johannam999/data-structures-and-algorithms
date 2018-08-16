95d6f88bb2fe49af8b1f1ccbcf54ab866c0f49b3
Joanna Coll <35988918+johannam999@users.noreply.github.com>
2018-08-15 21:45:40 -0700

    Update birthday-cake-candles.js

--- hacker-rank/birthday-cake-candles/birthday-cake-candles.js
+++ hacker-rank/birthday-cake-candles/birthday-cake-candles.js
@@ -30,7 +30,7 @@ function birthdayCakesCandles(ar) {
   let count = 0;
   for (let i = 0; i < ar.length; i++) {
     if (ar[i] === maxHeight) {
-      count++;  
+      count+=1;  
     }
   }
   return count;
